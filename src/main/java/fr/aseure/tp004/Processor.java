5a4ee952674ace92ad0c7e1e8ab4825a9796d277
Anthony Seure <anthony.seure@gmail.com>
2022-04-01 12:12:32 +0200

    tp004: add correction

---
+++ src/main/java/fr/aseure/tp004/Processor.java
@@ -0,0 +1,7 @@
+package fr.aseure.tp004;
+
+interface Processor {
+    void processLine(String line);
+
+    void report();
+}
---
+++ src/main/java/fr/aseure/tp004/SRP.java
@@ -0,0 +1,33 @@
+package fr.aseure.tp004;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class SRP {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String url = "https://en.wikipedia.org/wiki/Recursion_(computer_science)";
+
+        var downloader = new Downloader();
+        var body = downloader.downloadPage(url);
+        var doc = new Document(body);
+
+        var processors = Arrays.asList(
+                new CountOccurence("recursion"),
+                new MaxLineLength()
+        );
+        process(doc, processors);
+
+        for (Processor p : processors) {
+            p.report();
+        }
+    }
+
+    static void process(Document doc, List<Processor> processors) {
+        for (String line : doc.splitByLine()) {
+            for (Processor p : processors) {
+                p.processLine(line);
+            }
+        }
+    }
+}
