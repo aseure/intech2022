ecfe472660584f548cf5d211c9fa5904b2095a9e
Anthony Seure <anthony.seure@gmail.com>
2022-04-01 09:39:34 +0200

    tp003: add corrections for ex2 and ex3

---
+++ src/main/java/fr/aseure/tp003/ex2/Instrument.java
@@ -0,0 +1,7 @@
+package fr.aseure.tp003.ex2;
+
+public class Instrument {
+    public void play() {
+        System.out.println("Instrument");
+    }
+}
---
+++ src/main/java/fr/aseure/tp003/ex2/StringInstrument.java
@@ -0,0 +1,8 @@
+package fr.aseure.tp003.ex2;
+
+public class StringInstrument extends Instrument {
+    @Override
+    public void play() {
+        System.out.println("StringInstrument");
+    }
+}
